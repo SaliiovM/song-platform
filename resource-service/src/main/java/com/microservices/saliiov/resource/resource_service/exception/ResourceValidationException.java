0f446ecb7ce77306d034659e58eaba6b024b5325
Maksym Saliiov <maksym.saliiov@ext.xactus.com>
2024-06-27 13:20:05 +0300

    Revert "Microservices Architecture overview"
    This reverts commit 05f2f27eb8cd5244db6d806a5aa2b4d59e85c1a0.

--- resource-service/src/main/java/com/microservices/saliiov/resource/resource_service/exception/ResourceValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.microservices.saliiov.resource.resource_service.exception;
-
-public class ResourceValidationException extends RuntimeException {
-    public ResourceValidationException(String message) {
-        super(message);
-    }
-}
--- song-service/src/main/java/com/microservices/saliiov/song/song_service/exception/SongValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.microservices.saliiov.song.song_service.exception;
-
-public class SongValidationException extends RuntimeException {
-    public SongValidationException(String message) {
-        super(message);
-    }
-}
