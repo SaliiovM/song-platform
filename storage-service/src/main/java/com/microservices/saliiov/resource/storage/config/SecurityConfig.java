2fa3fd4574c678660251b1281eda97a4fb0ef46e
MSaliiov <maksym.saliiov@gmail.com>
2025-05-01 14:18:00 +0300

    [Security] fixed auth configuration

--- storage-service/src/main/java/com/microservices/saliiov/resource/storage/config/SecurityConfig.java
+++ storage-service/src/main/java/com/microservices/saliiov/resource/storage/config/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.microservices.saliiov.resource.storage.config;
 
 import com.microservices.saliiov.resource.storage.auth.CombinedAuthManager;
-import jakarta.ws.rs.HttpMethod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
